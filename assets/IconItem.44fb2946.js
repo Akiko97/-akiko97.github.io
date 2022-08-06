fef15435eff263ce2288d2ae72e58b9b1abcf2a7
Gabriel Zhong <gabriel@Gabriels-MacBook-Pro.local>
2022-08-06 20:31:59 +0900

    deploy

---
+++ assets/IconItem.44fb2946.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o,e as c}from"./index.5286f8a4.js";const s={},n={style:{width:"24px",height:"24px"},viewBox:"0 0 24 24"},r=c("path",{fill:"currentColor",d:"M12,1L9,9L1,12L9,15L12,23L15,15L23,12L15,9L12,1Z"},null,-1),_=[r];function a(i,l){return t(),o("svg",n,_)}const h=e(s,[["render",a]]);export{h as I};
